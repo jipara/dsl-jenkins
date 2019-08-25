3c0001cbe4f05405412d91d030229bfc4e49031e
jipara <zhypargul.esengulova@gmail.com>
2019-08-25 15:34:21 -0500

    ddd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -38,5 +38,31 @@ pipeline{
                 }
             }
         }
+        stage("Download Packer"){
+            steps{
+                ws("tmp/"){
+                    script {
+                        def exists = fileExists 'packer_1.4.3_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip"
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        }
+                    }
+                }
+            }
+        }
+        stage("Write to a file"){
+            steps{
+                ws("tmp/"){
+                    writeFile text: "Test",file "TestFile"
+                }
+            }
+        }    
     }
 }   
